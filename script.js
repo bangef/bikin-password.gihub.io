a021f7288dc108fd8e3e86ffa75a43177e29f501
bangef <ficrihnp@gmail.com>
2022-03-15 10:43:32 +0700

    benerin bug

--- script.js
+++ script.js
@@ -22,27 +22,33 @@ const checkbox = (a) => {
 const randPass = (a = checkbox(true), b = document.querySelector('#inpPanjPass')) => {
     let result = '';
     let arrWrapper = '';
-    const validasi = a != null && a != undefined && b.value != null && b.value != undefined && isNaN(b.value) != true;
-    if (a == 'angka' && validasi) arrWrapper = dataArr[0];
-    else if (a == 'lowcase' && validasi) arrWrapper = dataArr[1];
-    else if (a == 'uppercase' && validasi) arrWrapper = dataArr[2];
-    else if (a == 'angkalowcase' && validasi) arrWrapper = dataArr[0] + dataArr[1];
-    else if (a == 'angkauppercase' && validasi) arrWrapper = dataArr[0] + dataArr[2];
-    else if (a == 'lowcaseuppercase' && validasi) arrWrapper = dataArr[1] + dataArr[2];
-    else if (a == 'angkalowcaseuppercase' && validasi) arrWrapper = dataArr.join("");
-    else arrWrapper = '';
+    if (a === 'angka' && b.value !== '') arrWrapper = dataArr[0];
+    else if (a === 'lowcase' && b.value !== '') arrWrapper = dataArr[1];
+    else if (a === 'uppercase' && b.value !== '') arrWrapper = dataArr[2];
+    else if (a === 'angkalowcase' && b.value !== '') arrWrapper = dataArr[0] + dataArr[1];
+    else if (a === 'angkauppercase' && b.value !== '') arrWrapper = dataArr[0] + dataArr[2];
+    else if (a === 'lowcaseuppercase' && b.value !== '') arrWrapper = dataArr[1] + dataArr[2];
+    else if (a === 'angkalowcaseuppercase' && b.value !== '') arrWrapper = dataArr.join("");
+    else arrWrapper = [''];
 
     for (let i = 0; i < b.value; i++) {
         result += arrWrapper[Math.floor(Math.random() * arrWrapper.length)];
     }
-
+    // console.log(arrWrapper);
+    // console.log(result);
+    // console.log(b.value === '');
     return result;
 };
 
 
+
 $(document).ready(() => {
     $('#acakPass').click(() => {
-        $('#output').removeClass('hide').html(`<td colspan="2"><div class="parent">${randPass()}</div></td>`);
+        if (randPass() !== '') {
+            $('#output').removeClass('hide').html(`<td colspan="2"><div class="parent">${randPass()}</div></td>`);
+        } else {
+            $('#output').addClass('hide')
+        }
     });
     $('#reset').click(() => {
         checkbox(false);
